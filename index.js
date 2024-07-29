90738f73a5eeffa3e08ab7e1fd3f49502adc8ca7
Ruyher <joaquinmanuelmiravet@gmail.com>
2024-07-29 17:28:03 -0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,104 @@
-function prticion(params){
-    let result = document.querySelector(".result");
-    let input = document.querySelector("#numpok").value;
-    let html= ``;
-    fetch('https://pokeapi.co/api/v2/pokemon/'+input)
-        .then(response => response.json())
-        .then ((data) => {
-            console.log(data)
-            result.innerHTML = `<h1>${data.name}</h1> <h2>${data.types[0].type.name}</h2> <img src="${data.sprites.other.home.front_default}"/>`;
-        });
-}
+function solicitudAJAX() {
+    var url = "https://pokeapi.co/api/v2/pokemon?limit=900";
+    let tarjetas = document.querySelector("#nPokemon");
+    var objXMLHttpRequest = new XMLHttpRequest();
+  
+    objXMLHttpRequest.onreadystatechange = function () {
+      if (objXMLHttpRequest.readyState === 4) {
+        if (objXMLHttpRequest.status === 200) {
+          let json = JSON.parse(objXMLHttpRequest.responseText);
+          console.dir(json);
+
+          
+          tarjetas.data = json;
+          for (let i = 0; i < json.results.length; i++) {
+            buscarPorURL(json.results[i].url);
+          }
+        } else {
+          alert("Error Code: " + objXMLHttpRequest.status);
+          alert("Error Message: " + objXMLHttpRequest.statusText);
+        }
+      }
+    };
+    objXMLHttpRequest.open("GET", url);
+    objXMLHttpRequest.send();
+  }
+  
+  function buscarPorURL(urlPokemon) {
+    var objXMLHttpRequest = new XMLHttpRequest();
+    
+  
+    objXMLHttpRequest.onreadystatechange = function () {
+      let div = document.querySelector("#ConteinerCard");
+      if (objXMLHttpRequest.readyState === 4) {
+        if (objXMLHttpRequest.status === 200) {
+          let json = JSON.parse(objXMLHttpRequest.responseText);
+          let nombre = json.name;
+          let uriImg = json.sprites.other.home.front_default;
+  
+          let html =
+            `<div class="card" style="width: 18rem;">
+    <img src="` +
+            uriImg +
+            `" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">` +
+            nombre +
+            `</h5>
+      <p class="card-text"></p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+          div.innerHTML =html;
+          console.log(div);
+        } else {
+          alert("Error Code: " + objXMLHttpRequest.status);
+          alert("Error Message: " + objXMLHttpRequest.statusText);
+        }
+      }
+    };
+    objXMLHttpRequest.open("GET", urlPokemon);
+    objXMLHttpRequest.send();
+  }
+  
+  function buscar() {
+    let tarjetas = document.querySelector("#ConteinerCard");
+    var data = document.querySelector("#nPokemon").data;
+    var busqueda = document.querySelector("#nPokemon").value - 1;
+    var url = data.results[busqueda].url;
+  
+    if (busqueda >= 0) {
+      var objXMLHttpRequest = new XMLHttpRequest();
+  
+      objXMLHttpRequest.onreadystatechange = function () {
+        if (objXMLHttpRequest.readyState === 4) {
+          if (objXMLHttpRequest.status === 200) {
+            let json = JSON.parse(objXMLHttpRequest.responseText);
+            let nombre = json.name;
+            let uriImg = json.sprites.other.home.front_default;
+            let html =
+              `<div class="card" style="width: 18rem;">
+    <img src="` +
+              uriImg +
+              `" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">` +
+              nombre +
+              `</h5>
+      <p class="card-text"></p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+            tarjetas.innerHTML = html;
+          } else {
+            alert("Error Code: " + objXMLHttpRequest.status);
+            alert("Error Message: " + objXMLHttpRequest.statusText);
+          }
+        }
+      };
+      objXMLHttpRequest.open("GET", url);
+      objXMLHttpRequest.send();
+    } else {
+      alert("Ingrese un numero del 1 al 20 para obtener su pokemon.");
+    }
+  }
